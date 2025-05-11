a5c6e4e0beffef31dfb7b6f2187cc4f5a5b98fc5
Marcin Zub <marcin_zub@outlook.com>
2025-05-11 22:42:40 +0200

    fix: main layout fix
    in order for footer to stick to the bottom, main part of the page
    expands thus allowing the correct footer positioning

--- src/frontend/src/components/base/Base.js
+++ src/frontend/src/components/base/Base.js
@@ -1,29 +1,39 @@
-import React from 'react';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
-import Header from '../header/Header';
-import Footer from '../footer/Footer';
+import React from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
 
-function Base({ title = 'Cruise-literature', children }) {
-    return (
-        <HelmetProvider>
-            <>
-                <div className="min-h-screen bg-background-light text-light dark:bg-background-dark dark:text-dark p-8 transition-colors duration-300">
-                    <Helmet>
-                        <meta charSet="utf-8" />
-                        <title>{title}</title>
-                        <meta name="description" content="" />
-                        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-                        <meta name="viewport" content="width=device-width, initial-scale=1" />
-                        <link rel="stylesheet" type="text/html" href="/static/css/style.css" />
-                        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-                    </Helmet>
-                    <Header />
-                    {children}
-                    <Footer />
-                </div>
-            </>
-        </HelmetProvider>
-    );
+function Base({ title = "Cruise-literature", children }) {
+  return (
+    <HelmetProvider>
+      <>
+        <div className="flex flex-col min-h-screen bg-background-light text-light dark:bg-background-dark dark:text-dark transition-colors duration-300 ">
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>{title}</title>
+            <meta name="description" content="" />
+            <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+            <link
+              rel="stylesheet"
+              type="text/html"
+              href="/static/css/style.css"
+            />
+            <script
+              defer
+              src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"
+            ></script>
+          </Helmet>
+          <Header />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </div>
+      </>
+    </HelmetProvider>
+  );
 }
 
 export default Base;
