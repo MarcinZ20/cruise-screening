01e5892ba92dd89997f24c4b8f10f3804ad6f31e
Marcin Zub <marcin_zub@outlook.com>
2025-06-03 00:14:44 +0200

    fix: improve search ui
    search is now divided into list elements with proper styling and
    placement

--- src/frontend/src/components/document_search/card/Card.js
+++ src/frontend/src/components/document_search/card/Card.js
@@ -1,137 +1,160 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Card = ({ resultKey, searchResult }) => {
-    const [openAbstract, setOpenAbstract] = useState(false);
-    const [openKeywords, setOpenKeywords] = useState(false);
+  const [openAbstract, setOpenAbstract] = useState(false);
+  const [openKeywords, setOpenKeywords] = useState(false);
 
-    const truncateText = (text, length = 500) => {
-        return text.length > length ? text.substring(0, length) + '...' : text;
-    };
+  const truncateText = (text, length = 500) => {
+    return text.length > length ? text.substring(0, length) + "..." : text;
+  };
 
-    return (
-        <article className="card mb-4" x-data="{ openAbstract: false, openKeywords: false, showCSOKeywords: false}">
-            <div className="card-content pb-1">
-                <p className="title">
-                    {searchResult.url ? (
-                        <a href={searchResult.url}>{searchResult.title}</a>
-                    ) : (
-                        searchResult.title
-                    )}
-                    {searchResult.pdf && (
-                        <a href={searchResult.pdf}>
-                            <img src="pdf-icon.svg" width="25" height="25" alt="PDF" />
-                        </a>
-                    )}
-                </p>
-                <p className="subtitle mb-2">{searchResult.authors}</p>
-                <p>
-                    <span className="is-size-6 p-0 m-0 has-text-primary">
-                        {searchResult.publication_date} - {searchResult.venue}
-                    </span>
-                    {searchResult.doi && (
-                        <span className="has-text-grey-light is-size-7">
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;✦&nbsp;&nbsp;
-                            <a href={`https://doi.org/${searchResult.doi}`} target="_blank" rel="noopener noreferrer">
-                                {searchResult.doi}
-                            </a>
-                        </span>
-                    )}
-                </p>
+  return (
+    <article className="bg-gray-100 dark:bg-gray-500 mb-4 p-1 rounded-lg shadow-md">
+      <div className="card-content">
+        <div className="flex flex-row items-center justify-between p-1">
+          <div className="flex-1">
+            <p className="text-md font-medium dark:text-gray-100">
+              {(() => {
+                const titleWords = searchResult.title.split(" ");
+                const shortenedTitle =
+                  titleWords.length > 10
+                    ? `${titleWords.slice(0, 10).join(" ")} ...`
+                    : searchResult.title;
+
+                return searchResult.url ? (
+                  <a href={searchResult.url}>{shortenedTitle}</a>
+                ) : (
+                  shortenedTitle
+                );
+              })()}
+            </p>
+          </div>
+
+          {searchResult.pdf && (
+            <div className="flex-shrink-0 ml-2">
+              <a href={searchResult.pdf}>
+                <img src="pdf-icon.svg" width="25" height="25" alt="PDF" />
+              </a>
             </div>
+          )}
+        </div>
 
-            <div className="card-content pt-1">
-                <div className="content">
-                    {searchResult.abstract && searchResult.abstract !== truncateText(searchResult.abstract) ? (
-                        openAbstract ? (
-                            <p className="is-5">
-                                <strong>Abstract:</strong> {searchResult.abstract}
-                                <br />
-                                <button
-                                    type="button"
-                                    className="card__show-more"
-                                    onClick={() => setOpenAbstract(false)}
-                                >
-                                    Show less
-                                </button>
-                            </p>
+        <p className="subtitle px-1 mb-2 text-sm font-medium text-gray-600 dark:text-gray-300">
+          {searchResult.authors}
+        </p>
+        <p className="p-1 text-sm">
+          Published:{" "}
+          <span className="text-primary">
+            {searchResult.publication_date}, {searchResult.venue}
+          </span>
+        </p>
+        <p className="p-1 text-sm">
+          DOI:{" "}
+          {searchResult.doi && (
+            <a
+              href={`https://doi.org/${searchResult.doi}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-500 text-sm"
+            >
+              {searchResult.doi}
+            </a>
+          )}
+        </p>
+      </div>
 
-                        ) : (
-                            <p className="is-5">
-                                <strong>Abstract:</strong> {truncateText(searchResult.abstract)}
-                                <br />
-                                <button
-                                    type="button"
-                                    className="card__show-more"
-                                    onClick={() => setOpenAbstract(true)}
-                                >
-                                    Show full abstract
-                                </button>
-                            </p>
+      <div className="card-content pt-1">
+        <div className="content">
+          {searchResult.abstract ? (
+            !openAbstract ? (
+              <button
+                type="button"
+                className="text-orange-500 hover:underline mt-2 px-1"
+                onClick={() => setOpenAbstract(true)}
+              >
+                See abstract &rarr;
+              </button>
+            ) : (
+              <div>
+                <p className="text-sm">
+                  <strong>Abstract:</strong> {searchResult.abstract}
+                </p>
+                <button
+                  type="button"
+                  className="text-orange-500 hover:underline mt-1 px-1"
+                  onClick={() => setOpenAbstract(false)}
+                >
+                  Show less
+                </button>
+              </div>
+            )
+          ) : null}
 
-                        )
-                    ) : searchResult.abstract ? (
-                        <p className="is-5">
-                            <strong>Abstract:</strong> {searchResult.abstract}
-                        </p>
-                    ) : searchResult.snippet ? (
-                        <p className="is-5">
-                            <strong>Snippet:</strong> {searchResult.snippet}...
-                        </p>
-                    ) : (
-                        <p className="is-5">
-                            <strong>Abstract:</strong> No abstract available
-                        </p>
-                    )}
+          {/* Keywords - pokazuj tylko jeśli są */}
+          {(searchResult.keywords_snippet?.length > 0 ||
+            searchResult.keywords_rest?.length > 0) && (
+            <div className="mt-2">
+              {searchResult.keywords_snippet?.map(([keyword, score]) => (
+                <a
+                  key={keyword}
+                  href={`?search_query=${encodeURIComponent(keyword)}&source=keywords`}
+                  className={`button is-light is-small mb-2 ${score}`}
+                >
+                  {keyword}
+                </a>
+              ))}
 
-                    <p>
-                        {/* todo - potentially not working */}
-                        {searchResult.keywords_snippet?.map(([keyword, score]) => (
-                            <a
-                                key={keyword}
-                                href={`?search_query=${encodeURIComponent(keyword)}&source=keywords`}
-                                className={`button is-light is-small mb-2 ${score}`}
-                            >
-                                {keyword}
-                            </a>
-                        ))}
+              {searchResult.keywords_rest?.length > 0 && !openKeywords && (
+                <button
+                  type="button"
+                  className="text-blue-600 hover:underline mt-1"
+                  onClick={() => setOpenKeywords(true)}
+                >
+                  Show more keywords
+                </button>
+              )}
 
-                        {searchResult.keywords_rest?.length > 0 && !openKeywords && (
-                            <button type="button" className="card__show-more" onClick={() => setOpenKeywords(true)}>
-                                Show more
-                            </button>
-                        )}
-
-                        {openKeywords && (
-                            <>
-                                <span>
-                                    {/* todo - potentially not working */}
-                                    {searchResult.keywords_rest?.map(([keyword, score]) => (
-                                        <a
-                                            key={keyword}
-                                            href={`?search_query=${encodeURIComponent(keyword)}&source=keywords`}
-                                            className={`button is-light is-small mb-2 ${score}`}
-                                        >
-                                            {keyword}
-                                        </a>
-                                    ))}
-                                </span>
-                                <button type="button" className="card__show-more"
-                                    onClick={() => setOpenKeywords(false)}>
-                                    Show less
-                                </button>
-                            </>
-                        )}
-                    </p>
-
-                    {/* todo - there are no citations and references in searchResult */}
-                    <p className="has-text-info">
-                        Cited
-                        by {searchResult.citations} &nbsp;&nbsp; - &nbsp;&nbsp; {searchResult.references} references
-                    </p>
-                </div>
+              {openKeywords && (
+                <>
+                  <div>
+                    {searchResult.keywords_rest?.map(([keyword, score]) => (
+                      <a
+                        key={keyword}
+                        href={`?search_query=${encodeURIComponent(keyword)}&source=keywords`}
+                        className={`button is-light is-small mb-2 ${score}`}
+                      >
+                        {keyword}
+                      </a>
+                    ))}
+                  </div>
+                  <button
+                    type="button"
+                    className="text-blue-600 hover:underline mt-1"
+                    onClick={() => setOpenKeywords(false)}
+                  >
+                    Show less keywords
+                  </button>
+                </>
+              )}
             </div>
-        </article>
-    )
+          )}
+
+          {/* Citation info - pokazuj tylko jeśli są */}
+          {(searchResult.citations || searchResult.references) && (
+            <p className="text-sm text-blue-800 mt-2">
+              {searchResult.citations
+                ? `Cited by ${searchResult.citations}`
+                : ""}
+              {searchResult.citations && searchResult.references ? " - " : ""}
+              {searchResult.references
+                ? `${searchResult.references} references`
+                : ""}
+            </p>
+          )}
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default Card;
--- src/frontend/src/components/document_search/wikipedia_card/WikipediaCard.js
+++ src/frontend/src/components/document_search/wikipedia_card/WikipediaCard.js
@@ -1,34 +1,44 @@
-import React from 'react';
+import React from "react";
 
-const WikipediaCard = ({matchedWikiPage}) => {
-    return (
-        <article className="card my-4 card--wiki">
-            <div className="card-content">
-                {!matchedWikiPage.ambiguous && (
-                    <>
-                        <p className="subtitle">Multiple matches found. Did you mean:</p>
-                        <hr/>
-                    </>
-                )}
+const WikipediaCard = ({ matchedWikiPage }) => {
+  return (
+    <article className="bg-gray-100 p-2 my-4 rounded-lg shadow-md card--wiki">
+      <div className="card-content">
+        {!matchedWikiPage.ambiguous && (
+          <>
+            <p className="subtitle">Multiple matches found. Did you mean:</p>
+            <hr />
+          </>
+        )}
 
-                <p className="title">
-                    <a href={matchedWikiPage.url} target="_blank" rel="noopener noreferrer">
-                        {matchedWikiPage.title}
-                    </a>
-                </p>
+        <div className="flex flex-column items-center justify-between mb-1 py-2">
+          <p className="text-lg font-medium">
+            <a
+              href={matchedWikiPage.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {matchedWikiPage.title} |{" "}
+              <span className="text-gray-500">Wikipedia</span>
+            </a>
+          </p>
+          <img
+            src="wikipedia-logo.png"
+            width="24"
+            height="24"
+            alt="Wikipedia Logo"
+            className="m-2"
+          />{" "}
+        </div>
+      </div>
 
-                <p className="subtitle">
-                    <img src="wikipedia-logo.png" width="24" height="24" alt="Wikipedia Logo"/> Wikipedia
-                </p>
-            </div>
-
-            <div className="card-content">
-                <div className="content">
-                    <p>{matchedWikiPage.snippet}</p>
-                </div>
-            </div>
-        </article>
-    );
+      <div className="card-content">
+        <div className="content">
+          <p>{matchedWikiPage.snippet}</p>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default WikipediaCard;
